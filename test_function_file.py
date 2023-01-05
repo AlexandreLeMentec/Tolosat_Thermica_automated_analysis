2f0bb086cd87f6cf4418ed58b56a772e86977bfe
Alexandre Le Mentec <116634144+AlexandreLeMentec@users.noreply.github.com>
2023-01-05 19:44:24 +0100

    pandas lib adding

--- test_function_file.py
+++ test_function_file.py
@@ -1,10 +1,10 @@
 # the sole purpose of this file is to test things before implementing them in the main file
 # it's not intended to stay in the final product
+import pandas as pd
+import os
 
-def read_database():
-    with open("mission1_results\results.xls", "r") as database:
-        for line in database:
-            info = line.split()
-            print(info)
-
-read_database()
+def open_results(results_file):
+    list = pd.read_html(results_file)
+    df = pd.DataFrame(list[0])
+        
+open_results("results.html")
