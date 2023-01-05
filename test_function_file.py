f45932f01917b14d4f0b832216827645cb4c3c23
Alexandre Le Mentec <116634144+AlexandreLeMentec@users.noreply.github.com>
2023-01-05 18:29:54 +0100

    adding maxt and mint to database.csv

--- test_function_file.py
+++ test_function_file.py
@@ -1,13 +1,10 @@
 # the sole purpose of this file is to test things before implementing them in the main file
 # it's not intended to stay in the final product
 
-card_database = {}
-
 def read_database():
-    with open('database.csv', "r") as database:
+    with open("mission1_results\results.xls", "r") as database:
         for line in database:
             info = line.split()
-            card_database[info[0]] = [info[1], info[2], info[3]] # [thickness, dissipation, node]
-    print(card_database)
+            print(info)
 
 read_database()
